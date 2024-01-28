ac180f61825dad17c9fbd979671a1b96937be81a
Nathan <silentphoenix11@SilentPhoenix11.localdomain>
2024-01-28 00:46:34 -0500

    almost done setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import {puppyList} from './data.js'
-
+import { useState } from 'react'
 
 function App() {
+      console.log(puppyList)
+      const [puppies, setPuppies] = useState(puppyList)
+      return(
       <div>
       </div>
+      )
 }
 
 export default App
-export {puppyList}
+// export {puppyList}
