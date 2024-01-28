74bd0f7480d3c1d9aa976fef573efa60e203054c
Nathan <silentphoenix11@SilentPhoenix11.localdomain>
2024-01-28 09:02:34 -0500

    fixed up App

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import {puppyList} from './data.js'
 import { useState } from 'react'
 
 function App() {
-      console.log(puppyList)
-      const [puppies, setPuppies] = useState(puppyList)
-      return(
-      <div>
-      </div>
-      )
-}
+      console.log("puppyList", puppyList);
+      const [puppies, setPuppies] = useState(puppyList);
+      return (
+            <div className="App">
+              {
+                puppies.map((puppy) => {
+                     return <p key={puppy.id}>{puppy.name}</p>;
+                   })
+               }
+            </div>
+          );
+        }
 
 export default App
 // export {puppyList}
